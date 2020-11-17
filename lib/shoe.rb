cce810fe14eeb7ed8bcf23cc5dbd0014373e2e10
Adam Walker <adamw207@yahoo.com>
2020-11-17 23:21:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -45,34 +45,3 @@ end
 
 shoe = Shoe.new("new") 
 shoe.condition = 
-class Shoe
-  attr_accessor :condition
-
-  # we're initializing the attribute @condition, so we need to call the instance variable directly
-  def initialize(condition="new")
-    @condition = condition
-  end
-
-  # we're changing the value of @condition, so we call the writer method, which needs to be called on self or else Ruby thinks we're just setting a local variable
-  def wear
-    self.condition = "worn"
-  end
-
-  def cobble
-    self.condition = "new"
-  end
-
-  # we're just reading the value of @condition, so we can call the reader method (whose implicit receiver is self)
-  def report_condition
-    if condition == "new"
-      "This shoe is in perfect condition!"
-    elsif condition == "worn"
-      "This shoe needs to be cobbled!"
-    else
-      "The condition of this shoe is #{condition}."
-    end
-  end
-
-end
-Note that you can also use sel
-
