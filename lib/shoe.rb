8e9f40e586d4f8d8486d09123dbda3c0983fc30c
Adam Walker <adamw207@yahoo.com>
2020-11-17 22:35:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+class Shoe 
+  attr_accessor :color, :size, :material, :condition 
+  attr_reader :brand 
+  
+  def intialize(brand) 
+    @brand = brand 
+  end 
+  
+  def cobble 
+    puts "the shoe has been repaired" 
+    
